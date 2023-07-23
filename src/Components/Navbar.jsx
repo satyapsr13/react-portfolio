e8e3eacb3da80a7b49454d0b0a2cb78653fd23a1
satyapsr13 <satyapsr13@gmail.com>
2023-07-24 00:19:19 +0530

    latest build

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
   return (
     <div className="flex  justify-between items-center w-full h-20 text-white bg-black fixed px-4">
       <div>
-        <h1 className="font-signature text-5xl  ">Nikki</h1>
+        <h1 className="font-signature text-5xl  ">Satyapsr</h1>
       </div>
       <ul className="hidden md:flex">
         {links.map(({ id, link }) => (
